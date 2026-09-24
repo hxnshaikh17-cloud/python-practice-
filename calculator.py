019edbeed3d267a1b6a5e2b2cbc6493e75f89974
hxnshaikh17-cloud <hxnshaikh17@gmail.com>
2026-09-24 20:14:42 +0530

    Fixed division by zero bug and added float support

--- calculator.py
+++ calculator.py
@@ -1,7 +1,20 @@
-a=int(input("enter the first nunber"))
-b=int(input("enter the second number"))
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-
+a = float(input("enter the first number :"))
+b = float(input("enter the second number :"))
+print("write add for addition:")
+print("write sub for subtarction:")
+print("write div for division:")
+print("write multi for multiplication:")
+c = input('')
+if c == "add":
+    print (a+b)
+elif c == "sub":
+    print(a-b)
+elif c== "div":
+    if b ==0 :
+       print("invalid value")
+    else:
+       print(a/b)
+elif c == "multi":
+    print(a*b)
+else:
+    print("Invalid operation")
